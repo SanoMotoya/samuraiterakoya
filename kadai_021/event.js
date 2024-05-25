4e3554bee2f74f979fa31bbd795fa45c7c1688ec
SanoMotoya <165794122+SanoMotoya@users.noreply.github.com>
2024-05-25 20:18:58 +0900

    onigouri

---
+++ kadai_021/event.js
@@ -0,0 +1,9 @@
+const Btn = document.getElementById('btn');
+
+const Text = document.getElementById('text');
+
+Btn.addEventListener('click', () => {
+    setTimeout(() => {
+    text.textContent ='ボタンをクリックしました'
+},2000);
+});
