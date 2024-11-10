1cfce855552adcc2f96fd0b70cde1a2e56eb10b8
SanoMotoya <165794122+SanoMotoya@users.noreply.github.com>
2024-11-10 21:18:29 +0900

    Add files via upload

---
+++ engbodycopy/js/js.js
@@ -0,0 +1,15 @@
+
+
+// FAQのスライド
+
+$('.answer').hide();
+
+$(function() {
+  // タイトルをクリックすると
+  $('.question').click(function () {
+    // クリックした次の要素(コンテンツ)を開閉
+ $(this).next('.answer').slideToggle();
+ $(this).toggleClass("open");
+  });
+});
+
